c29ab7a0438a36d3142ee7ddaf560782dacfd188
larissagabriela <larissagosantana@gmail.com>
2022-03-31 15:56:19 -0300

    Aula3

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+        double salario;
+        salario = 3000.10;
+
+        salario = 3000;
+
+        double idade;
+
+        idade = 7.0 / 5;
+
+        Console.WriteLine(idade);
+
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+
+    }
+}
